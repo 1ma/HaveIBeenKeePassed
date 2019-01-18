bb47b2f48e9eaa0bdb7639a32541e040d3782671
Marcel Hernandez <e6990620@gmail.com>
2019-01-18 20:37:47 +0100

    rush to full orchestra in main body

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"github.com/1ma/HaveIBeenKeePassed/sax"
 	"github.com/1ma/HaveIBeenKeePassed/types"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,6 +24,40 @@ func main() {
 	go sax.Parse(file, c)
 
 	for entry := range c {
-		fmt.Printf("%+v\n", entry)
+		password := entry.Password
+
+		jash := sha1.Sum([]byte(password))
+		reference := strings.ToUpper(hex.EncodeToString(jash[:]))
+		prefics := strings.ToUpper(hex.EncodeToString(jash[:3])[:5])
+
+		url := fmt.Sprintf("https://api.pwnedpasswords.com/range/%s", prefics)
+
+		client := http.DefaultClient
+		req, _ := http.NewRequest("GET", url, nil)
+		req.Header.Set("User-Agent", "HaveIBeenKeePassed")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+
+		scanner := bufio.NewScanner(resp.Body)
+
+		compromized := false
+		for scanner.Scan() {
+			fullHash := prefics + scanner.Text()[:35]
+			// fmt.Println(fullHash)
+
+			if reference == fullHash {
+				compromized = true
+				fmt.Printf("%s: COMPROMiZED (%s)\n", entry.Title, entry.Password)
+			}
+		}
+
+		if compromized == false {
+			fmt.Println("%s: SAFE\n", entry.Title)
+		}
 	}
 }
