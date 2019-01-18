edf5f60848e6376014e71ea8c07a1922f5de4019
Marcel Hernandez <e6990620@gmail.com>
2019-01-18 23:26:33 +0100

    herp

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) != 2 {
 		fmt.Printf("Usage: %s filepath.xml\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	file, err := os.Open(os.Args[1])
-	defer file.Close()
-
 	if err != nil {
 		fmt.Printf("%s: No such file\n", os.Args[1])
 		os.Exit(1)
 	}
+
+	defer file.Close()
 
 	c := make(chan types.Entry, 64)
 
